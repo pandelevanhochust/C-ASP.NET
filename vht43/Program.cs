7a7a26e7c7ca805186b6cced1db9c0c3d021f181
pandelevanhochust <Toan.LV225160@sis.hust.edu.vn>
2026-03-06 18:04:33 +0700

    event

--- vht43/Program.cs
+++ vht43/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace VHT;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VHT;
 
 public class Program
 {
@@ -13,8 +15,21 @@ public class Program
         Console.WriteLine(sq(10));
         Console.WriteLine(cube(10));
         Console.WriteLine(sq.Target);
-        Console.WriteLine(cube.Target);
+        Console.WriteLine(cube.Target); 
         
+        // MultiDelegate md = new MultiDelegate();
+        // md.MultiCast();
+
+        // publisher
+        UserInput userevent = new UserInput();
+
+        userevent.Input();
+        
+        Square square = new Square();
+
+        square.Sub(userevent);
+        // subscriber
+         
     }
 
     delegate int Test(int x);
